39c1ce0cb12ae46df39795335ae5907cc15a7ed6
lukakharkhelauri <148798486+lukakharkhelauri@users.noreply.github.com>
2024-10-16 19:10:42 +0400

    Add files via upload

---
+++ homework/src/Main.java
@@ -0,0 +1,46 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //exercise 1
+//        int a = 10;
+//        int b = 20;
+//        int c = a;
+//        a = b;
+//        b = c;
+//
+//        System.out.println(" a = " + a + ", b = " + b);
+
+        //exercise 2
+//        float a = 10.234f;
+//        int b = 10;
+//        float c = a-b;
+//        System.out.format("%.3f %n", c); //stackoverflow-ზე ვნახე ესეთი ამოხსნა რადგან ლოგში პრინტავდა 0.2340002
+//        System.out.println(c); //ესე პრინტავდა
+
+        //exercise 3
+//        int number = 10000;
+//
+//        if (number >= 100 && number <= 999) {
+//            System.out.println("your number is 3 digits");
+//        } else if (number >= 999 || number <= 100) {
+//            System.out.println("your number not 3 digits");
+//        }
+
+//        System.out.println(number);
+
+        //exercise 5
+//        double number = 10.234;
+//        int number2 = (int) number;
+//        System.out.println(number2);
+
+//        //exercise 7
+//        int number = 2665;
+//        int seconds = number % 60;
+//        int minutes = (number % 3600) / 60;
+//        int hours = number / 3600;
+//        System.out.println(hours + ":" + minutes + ":" + seconds);
+
+    }
+}
